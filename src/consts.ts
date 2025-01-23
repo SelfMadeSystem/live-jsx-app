bac4cd43ec3d81eab0222d5afcdb27820977ab6f
SelfMadeSystem <sms@shoghisimon.cc>
2025-01-23 13:19:27 -0500

    Update default stuff

--- src/consts.ts
+++ src/consts.ts
@@ -3,16 +3,27 @@ import { useState } from 'react';
 
 export default function App() {
   const [count, setCount] = useState<number>(0);
-  const overTen = count > 10;
+  const geTen = count >= 10;
 
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-100">
+    <div className="flex flex-col items-center justify-center bg-gray-100"
+      style={{
+        '--n': count - 9
+      } as React.CSSProperties}>
       <p className="text-2xl font-bold mb-4">{count}</p>
-      <button 
-        className={\`fancy-btn px-4 py-2 text-white rounded \${overTen ? "uwuness" : ""}\`}
+      <div className={\`seperator-top \${geTen ? "ge-ten" : ""}\`}></div>
+      <button
+        className={\`fancy-btn px-4 py-2 text-white rounded \${geTen ? "ge-ten" : ""}\`}
         onClick={() => setCount(count + 1)}
       >
         Increment
+      </button>
+      <div className={\`seperator \${geTen ? "ge-ten" : ""}\`}></div>
+      <button
+        className={\`fancy-btn px-4 py-2 text-white rounded \${geTen ? "ge-ten" : ""}\`}
+        onClick={() => setCount(count - 1)}
+      >
+        Decrement
       </button>
     </div>
   );
@@ -21,9 +32,12 @@ export default function App() {
 
 export const DEFAULT_CSS = /*css*/ `\
 .fancy-btn {
-  transition: all 0.2s;
+  transition: background-position 0.2s,
+    --s1 0.3s,
+    --s2 1s;
   background: linear-gradient(90deg, #007cf0, #00dfd8);
   background-size: 200% auto;
+  transform: scale(calc(var(--s1) * var(--s2)));
 }
 
 .fancy-btn:hover {
@@ -31,6 +45,46 @@ export const DEFAULT_CSS = /*css*/ `\
 }
 
 .fancy-btn:active {
-  transform: scale(0.95);
+  --s1: 0.93;
+}
+
+@property --s1 {
+  syntax: '<number>';
+  inherits: true;
+  initial-value: 1;
+}
+
+@property --s2 {
+  syntax: '<number>';
+  inherits: true;
+  initial-value: 1;
+}
+
+@property --n {
+  syntax: '<number>';
+  inherits: true;
+  initial-value: 1;
+}
+
+.fancy-btn.ge-ten {
+  --s2: calc(var(--n) * 0.1 + 1);
+}
+
+.seperator {
+  transition: height 1s;
+  height: 0px;
+}
+
+.seperator.ge-ten {
+  height: calc(var(--n) * 4px);
+}
+
+.seperator-top {
+  transition: height 1s;
+  height: 0px;
+}
+
+.seperator-top.ge-ten {
+  height: calc(var(--n) * 2px);
 }
 `;
